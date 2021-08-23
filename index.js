68a5f1729746d93b5fe4be4528c60519601bbbdd
victor castro <victor_castro_01@hotmail.com>
2021-08-22 22:43:11 -0400

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(collection, argument){
+    return collection.filter(function(name){return name.toLowerCase()===argument.toLowerCase()});   
+}
+
+function fuzzyMatch(collection,letter){
+    return collection.filter(function(startsWith){return startsWith.startsWith(letter)})
+}
+
+function matchName(collection,str){    
+    return collection.filter(function(arg){return arg.name === str})
+}
